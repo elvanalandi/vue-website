b2651c242edb8ed9ab13bd62b07fe4f41c5ea82d
elvanalandi <37257641+elvanalandi@users.noreply.github.com>
2020-12-13 18:14:51 +0700

    Add files via upload

---
+++ vue-typescript/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <UserForm/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import UserForm from './components/UserForm.vue';
+
+@Component({
+  components: {
+    UserForm,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  background-color: #4caf4c;
+}
+div .user{
+  border: 10px solid whitesmoke;
+  margin: 0 auto;
+  padding: 10px;
+  border-radius: 10px;
+  width: 30%;
+  background-color: white;
+}        
+input {
+  padding: 15px 32px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+}
+
+button {
+  background-color: #4caf4c;
+  border: none;
+  border-radius: 10px;
+  margin: 0 auto;
+  color: white;
+  font-size: 16px;
+  text-align: center;
+  padding: 15px 32px;
+}
+</style>
---
+++ vue-typescript/src/components/UserForm.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="user">
+    <h1>User Form</h1>
+    <input type="number" v-model="idForm" placeholder="NIM/NIP">
+    <input type="text" v-model="nameForm" placeholder="Name">
+    <br>
+    <input type="radio" v-model="roleForm" name="roleForm" value="0"> Admin
+    <input type="radio" v-model="roleForm" name="roleForm" value="1"> Mahasiswa
+    <button v-on:click="showToConsole()">Submit</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  interface User {
+    id: number;
+    nama: string;
+    role: Role;
+  }
+  enum Role{
+      ADMIN = "ADMIN",
+      MAHASISWA = "MAHASISWA"
+  }
+
+  abstract class BaseUser implements User{
+      public nama = "";
+      public abstract role: Role;
+  }
+
+  class Mahasiswa extends BaseUser{
+      public nim = 0;
+      public role: Role = Role.MAHASISWA;
+  }
+
+  class Admin extends BaseUser{
+      public nip = 0;
+      public role: Role = Role.ADMIN;
+  }
+
+  @Component
+  export default class UserForm extends Vue {
+    private nameForm = "";
+    private idForm = "";
+    private roleForm = "";
+    public showToConsole(): void {
+      if (this.roleForm == "0"){
+        const admin: Admin = new Admin();
+        admin.nama = this.nameForm;
+        admin.nip = this.idForm;
+        admin.role = Role.ADMIN;
+        console.log("Admin",admin);
+      }else if(this.roleForm == "1"){
+        const mahasiswa: Mahasiswa = new Mahasiswa();
+        mahasiswa.nama = this.nameForm;
+        mahasiswa.nim = this.idForm;
+        mahasiswa.role = Role.MAHASISWA;
+        console.log("Mahasiswa",mahasiswa);
+      }else{
+        alert("Choose Role!");
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
