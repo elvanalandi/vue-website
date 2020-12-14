cfc235a2efde5754420f36c563fc11731683fce3
elvanalandi <elvanalandi@gmail.com>
2020-12-14 10:30:55 +0700

    new vue-typescript

---
+++ vue-typescript/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+body {
+  background-color: #4caf4c;
+}
+div .user{
+  border: 10px solid whitesmoke;
+  margin: 0 auto;
+  padding: 10px;
+  border-radius: 10px;
+  width: 30%;
+  background-color: white;
+}        
+input {
+  padding: 15px 32px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+}
+
+button {
+  background-color: #4caf4c;
+  border: none;
+  border-radius: 10px;
+  margin: 0 auto;
+  color: white;
+  font-size: 16px;
+  text-align: center;
+  padding: 15px 32px;
+}
+</style>
---
+++ vue-typescript/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <UserForm/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import UserForm from '@/components/UserForm.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    UserForm,
+  },
+})
+export default class Home extends Vue {}
+</script>
