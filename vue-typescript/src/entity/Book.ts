1dec79eefb296ae41d07b4e5bcb6a549edc8249a
elvanalandi <elvanalandi@gmail.com>
2020-12-16 21:00:15 +0700

    Update Book.ts

---
+++ vue-typescript/src/entity/Book.ts
@@ -0,0 +1,26 @@
+import { deserialize, inheritSerialization, serialize } from "cerialize";
+import BaseEntity from "./BaseEntity";
+
+@inheritSerialization(BaseEntity)
+export default class Book extends BaseEntity{
+
+    @deserialize
+    @serialize
+    public name: string = "";
+
+    @deserialize
+    @serialize
+    public description: string = "";
+
+    @deserialize
+    @serialize
+    public isbn: string = "";
+
+    @deserialize
+    @serialize
+    public author: string = "";
+
+    @deserialize
+    @serialize
+    public publisher: string = "";
+}
