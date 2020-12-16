d9d1f8782d68b053cd727fdb579b667bb421a511
elvanalandi <elvanalandi@gmail.com>
2020-12-16 20:59:13 +0700

    Update BaseEntity.ts

--- vue-typescript/src/entity/BaseEntity.ts
+++ vue-typescript/src/entity/BaseEntity.ts
@@ -1,6 +1,11 @@
 import { deserialize, serialize, Deserialize, Serialize } from "cerialize";
 
 export default abstract class BaseEntity {
+
+    @serialize
+    @deserialize
+    public id: number = 0;
+
     public serialize(){
         return Serialize(this);
     }
